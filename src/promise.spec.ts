d26501197e4035fac76008e8186046c105b8ca53
silberneimer <silbern.eimer@gmail.com>
2020-02-03 12:59:37 +0900

    Reformat promise spec code

--- src/promise.spec.ts
+++ src/promise.spec.ts
@@ -9,17 +9,19 @@ describe('Promise', () => {
     });
     it('then', () => {
         expect.assertions(1);
-        helloWorld().then(r => expect(r).toMatch('Hello, World!'))
+        helloWorld()
+            .then(r => expect(r).toMatch('Hello, World!'))
     });
     it('catch', () => {
         expect.assertions(1);
-        shouldBeError().catch((e) => {
-            expect(e as Error).toStrictEqual(ExpectError);
-        })
+        shouldBeError()
+            .catch((e) => {
+                expect(e as Error).toStrictEqual(ExpectError);
+            })
     });
 });
 
-const ExpectError = new Error("expect error")
+const ExpectError = new Error("expect error");
 
 function helloWorld(): Promise<string> {
     return new Promise<string>((resolve) => {
@@ -28,7 +30,7 @@ function helloWorld(): Promise<string> {
 }
 
 function shouldBeError(): Promise<string> {
-    return new Promise<string>((resolve, reject) => {
+    return new Promise<string>((_, reject) => {
         reject(ExpectError)
     })
 }
